d64f325a25930135ce5b636ba6d50f7264dd1ee0
Hungry_munk <abbaszadeharsam@gmail.com>
2022-11-01 13:52:16 +1100

    basic claculations now work

---
+++ script.js
@@ -0,0 +1,94 @@
+const numberButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator')
+const eqaulButton = document.querySelector('.equal')
+
+const previousOperation = document.querySelector('.previous-operation')
+const currentOperation = document.querySelector('.current-operation')
+
+const equation = {
+    currentOperation : '',
+    previousOperation: '',
+    operator : '',
+    answer: ''
+}
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', event =>{
+        if (button.value === "0" && !equation.currentOperation) return
+        equation.currentOperation += button.value
+        currentOperation.textContent = equation.currentOperation
+    })
+})
+
+operatorButtons.forEach(button=>{
+    button.addEventListener('click', event=>{
+        if(!equation.currentOperation || (equation.operator && !equation.currentOperation)) return // if they havent chosen a number first
+
+        if (equation.operator) {
+            calcualte()
+            updateCalculationDisplay(button)
+            return
+        }
+
+
+
+        equation.previousOperation = equation.currentOperation
+        equation.operator = button.value
+        equation.currentOperation = ''
+
+        previousOperation.textContent = `${equation.previousOperation} ${equation.operator}`
+        currentOperation.textContent = ""
+
+    })
+})
+
+eqaulButton.addEventListener('click', event =>{
+    if (equation.currentOperation && equation.previousOperation){
+        calcualte()
+        updateCalculationDisplay(event.target)
+    }
+
+})
+
+function updateCalculationDisplay(button) {
+    if (button.value === "=") {
+        console.log('worls')
+        //updating display
+        currentOperation.textContent = equation.answer
+        previousOperation.textContent = ""
+        // updating vlaues
+        equation.currentOperation = equation.answer
+        equation.previousOperation = ""
+        equation.operator = ""
+    } else {
+        previousOperation.textContent = `${equation.answer} ${equation.operator}`
+
+    }
+}
+
+function calcualte() {
+    let firstNumber = parseFloat(equation.previousOperation)
+    let secondNumber = parseFloat(equation.currentOperation)
+    let operator = equation.operator
+    let answer;
+
+    switch (operator) {
+        case '+':
+            answer = firstNumber + secondNumber
+            break
+        case '÷':
+            answer = firstNumber / secondNumber
+            break
+        case '^':
+            answer = firstNumber ** secondNumber
+            break
+        case 'X' :
+            answer = firstNumber * secondNumber
+            break
+        case '-':
+            answer = firstNumber - secondNumber
+            break;
+    }
+
+    equation.answer = answer
+}
